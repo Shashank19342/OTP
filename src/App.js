54fd1586305412f1125ae1725ce1a9cb7e5c5bf3
Shashank19342 <55043355+Shashank19342@users.noreply.github.com>
2021-12-02 11:38:18 +0530

    First Deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Navbar from "./Navbar";
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="otp">
+      <Navbar />
+      <div className="frame89">
+        <div className="frame85">
+          <form className="frame92">
+            <div className="group106">
+              <h1><i class="fas fa-arrow-left"></i></h1>
+              <div className="group105">
+                <h1>Verify your email</h1>
+                <div className="frame91">
+                  <p>A verification code has been sent to abc@gmail.com</p>
+                </div>
+                <div className="group104">
+                  <p>Verification Code</p>
+                  <input type="text" placeholder="6-digit verification code" />
+                </div>
+                <p>Didn’t receive the code? <span style={{color:"blue"}}>Resend</span></p>
+              </div>
+            </div>
+            <button className="frame82" type="submit">Verify Email</button>
+          </form>
+          <div className="group102">
+            <div className="frame88">
+              <div className="group101">
+                <h1>Why choose SubSpace?</h1>
+                <p>2000+ businesses trust their payments with SubSpace</p>
+              </div>
+              <div className="group99">
+                <div className="group98">
+                  <ul>
+                    <li style={{background: "url('https://cdn.vox-cdn.com/thumbor/AwKSiDyDnwy_qoVdLPyoRPUPo00=/39x0:3111x2048/1400x1400/filters:focal(39x0:3111x2048):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                    <li style={{background: "url('https://store-images.s-microsoft.com/image/apps.32357.14618985536919905.4b30e4f3-f7a1-4421-840c-2cc97b10e8e0.6e7a96f1-33ad-4447-8e2b-7de730f9a05e?mode=scale&q=90&h=200&w=200&background=%230078D7')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                    <li style={{background: "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAiLHlQFacU7SFa6qM7haLJGcX7RZXpDJ0TyXL8mkT-xaczuFlEfGHyPjVd7fxc7H342M&usqp=CAU')", backgroundSize: "cover"}}></li>
+                    <li style={{background: "url('https://origin-staticv2.sonyliv.com/UI_icons/fallback_sonyliv_logo.png')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                  </ul>
+                </div>
+                <div className="group98">
+                  <ul>
+                    <li style={{background: "url('https://upload.wikimedia.org/wikipedia/commons/5/5a/Zee5-official-logo.jpeg')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                    <li style={{background: "url('https://images-na.ssl-images-amazon.com/images/I/615NT%2B68MjL.png')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                    <li style={{background: "url('https://play-lh.googleusercontent.com/jFi2iC10wQJ42gu-DO2CMeIcN3qcmNQHtY5EBT_wtp4jCIozS4n3Q9pA7ZloDUGHHw')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                    <li style={{background: "url('https://upload.wikimedia.org/wikipedia/commons/b/b3/Aha_OTT_Logo.svg')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                  </ul>
+                </div>
+                <div className="group98">
+                  <ul>
+                    <li style={{background: "url('https://file.milkbasket.com/logos/app-logo.jpeg')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                    <li style={{background: "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR91k4eBg2jgNeQQkw83cKm_fQBfoDnyr2R5GDzU0y9gG85IkehUS6yo2_ExZ4u25a4vhw&usqp=CAU')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                    <li style={{background: "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWyqXPxYOi5keh763Be2W1THomPYn5KryfT5ofG7KYzxDvEUZ7waN1Z0HiZCum3u0W_A8&usqp=CAU')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                    <li style={{background: "url('https://www.androidfreeware.net/img2/cure-fit-healthy-food-fitness-yoga-meditation.jpg')", backgroundSize: "cover", backgroundPosition: "center"}}></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="group100">
+                <p>Need help? We are just a click away. <b><span style={{borderBottom:"1px solid #AEA398"}}>Contact Us</span></b></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+
+function Navbar(){
+    return(
+        <div className="frame87">
+            <h1 className="logo">Subspace.</h1>
+            <div className="frame86">
+                <p>Already a user?</p>
+                <button className="frame83">Log In</button>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
+
+
